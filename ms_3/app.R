8719291a8baf3c9dd46e3a042a37ec7f85d64846
danielate <dteran@mde.harvard.edu>
2020-02-28 21:53:51 -0500

    Second Commit - Datasets in ms_3_raw-data folder

--- ms_3/app.R
+++ ms_3/app.R
@@ -1,10 +1,29 @@
 
 
 ui <- navbarPage(
-    "Final Project Title",
+    "Gender inequality for Women in the Informal Economy in Latin America",
+    tabPanel("About", 
+             titlePanel("About"),
+             h3("Project Background and Motivations"),
+             p("Identity has several dimensions, including gender, race, sexuality, and class, 
+             and these intersecting links influence a person’s daily life.
+             Therefore, a gender analysis that utilizes an intersectional lens 
+             is essential to designing and implementing equitable international 
+             development projects. This project aims to compare markers of gender 
+             inequality with data on women workers in the informal economy in Latin A
+             merica, with the goal of analyzing how this already vulnerable group is 
+             restricted by issues of race, class, gender and culture.
+"),
+             h3("About Me"),
+             p("My name is Daniela Teran and I am a Masters in Design Engineering candidate, 2020. 
+             You can reach me at dteran@mde.harvard.edu.")),
+    tabPanel("Discussion",
+             titlePanel("Discussion Title"),
+             p("Tour of the modeling choices you made and 
+              an explanation of why you made them")),
     tabPanel("Model",
              fluidPage(
-                 titlePanel("Model Title"),
+                 titlePanel("Access to education"),
                  sidebarLayout(
                      sidebarPanel(
                          selectInput(
@@ -13,18 +32,7 @@ ui <- navbarPage(
                              c("Option A" = "a", "Option B" = "b")
                          )),
                      mainPanel(plotOutput("line_plot")))
-             )),
-    tabPanel("Discussion",
-             titlePanel("Discussion Title"),
-             p("Tour of the modeling choices you made and 
-              an explanation of why you made them")),
-    tabPanel("About", 
-             titlePanel("About"),
-             h3("Project Background and Motivations"),
-             p("Hello, this is where I talk about my project."),
-             h3("About Me"),
-             p("My name is ______ and I study ______. 
-             You can reach me at ______@college.harvard.edu.")))
+             )))
 
 
 server <- function(input, output) {
